9a52c0d68f26df2fd45a4de1a675cd8477df00a5
juan ortega <jortegah79@gmail.com>
2025-09-21 11:37:06 +0200

    predenred from server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+  path:'pokemons/page/:id',    
+  renderMode: RenderMode.Server
+  },
+  {
     path: 'pokemon/:id',
     renderMode: RenderMode.Server  // Renderizado bajo demanda
   },
